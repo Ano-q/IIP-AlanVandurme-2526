a49d07ded3a1152c0b5f81f1260d0e41a0df5c76
Ano-q <alan.vandurme@student.odisee.be>
2025-09-24 11:56:49 +0200

    ConsoleHalloWereld

---
+++ IIP1.01.Basis/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleHalloWereld
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+		Console.ForegroundColor = ConsoleColor.Yellow; // Yellow tekstkleur
+        Console.WriteLine("Hallo wereld");
+		Console.ResetColor();
+      }
+   }
+}
