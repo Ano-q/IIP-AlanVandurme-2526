b7921276efd988516ecc532e6f867147814b81a8
Ano-q <alan.vandurme@student.odisee.be>
2025-09-25 12:34:17 +0200

    ConsoleGangsterName
    Oefening les1: ConsoleGangsterName

---
+++ IIP1.01.Basis/ConsoleGangsterName/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ConsoleGangsterName
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+        Console.WriteLine("****************************");
+        Console.WriteLine("| GANGSTA NAME BUILDER |");
+        Console.WriteLine("****************************");
+        Console.WriteLine();
+		Console.WriteLine();
+		Console.WriteLine();
+		
+		Console.Write("Give the first name of any Disney character: ");
+		string character = Console.ReadLine();
+		
+		Console.Write("Give any workbench tool: ");
+		string tool = Console.ReadLine();
+		
+		Console.Write("What is your last name: ");
+		string name = Console.ReadLine();
+		
+		string result = $"{character} 'the {tool}' {name}";
+		Console.WriteLine();
+		Console.Write("Your gangsta name: ");
+		Console.ForegroundColor = ConsoleColor.Green; // Green tekstkleur
+		Console.Write(result);
+      }
+   }
+}
