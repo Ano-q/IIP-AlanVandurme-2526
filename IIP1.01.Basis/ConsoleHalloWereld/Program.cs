7c28314f142fa8db3079d7593afec6a9b85b2810
Ano-q <alan.vandurme@student.odisee.be>
2025-09-25 18:06:54 +0200

    Correctie ConsoleHalloWereld
    Nieuwe versie van ConsoleHalloWereld.

--- IIP1.01.Basis/ConsoleHalloWereld/Program.cs
+++ IIP1.01.Basis/ConsoleHalloWereld/Program.cs
@@ -6,9 +6,16 @@ namespace ConsoleHalloWereld
    {
       static void Main(string[] args)
       {
-		Console.ForegroundColor = ConsoleColor.Yellow; // Yellow tekstkleur
-        Console.WriteLine("Hallo wereld");
-		Console.ResetColor();
+		Console.WriteLine("Hallo Wereld!");
+        Console.Write("Hoe heet je: ");
+        string naam = Console.ReadLine();
+		
+		Console.ForegroundColor = ConsoleColor.Yellow;
+		Console.WriteLine($"Aangename kennismaking, {naam}");
+        Console.ResetColor();
+		
+		Console.WriteLine("druk een toets om verder te gaan...");
+        Console.ReadKey();
       }
    }
 }
