9c57808a37a73a6e9c9f856e979b427dd6fe152b
Ano-q <alan.vandurme@student.odisee.be>
2025-10-05 17:56:46 +0200

    ConsoleDrankkaart
    les 01 ConsoleDrankkaart

---
+++ IIP1.01.Basis/ConsoleDrankkaart/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleDrankkaart
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+		Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("@====== MENU ======");
+        Console.WriteLine("☕  Koffie:	3€");
+        Console.WriteLine("🍵  Thee:	2€");
+        Console.WriteLine("🥤  Limo:	2€");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("copyright ©2025");
+        Console.WriteLine();
+        Console.WriteLine("Het menu staat ook op \"C:\\Dropbox\\MijnCafe\\menu.pdf\"");
+      }
+   }
+}
